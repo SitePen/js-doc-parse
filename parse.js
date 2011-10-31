975722e2de0b5f702416eeda69875fbfc94492cf
Colin Snover <github.com@zetafleet.com>
2011-10-31 01:13:23 +0000

    Fix many bugs. 1. Disallow instance creation of objects without new keyword 2. Fix doc parser cannot parse itself due to es5 getter/setter 3. Make log messages clearer 4. Add some debugging stuff to investigate cases where arrays of arrays get sent to getVariable 5. Fix incorrect lookups of properties matching Object.prototype 6. Fix binary operation contexts were getting mashed together during recursion 7. Add some other temporary debugging stuff

--- parse.js
+++ parse.js
@@ -1,3 +1,22 @@
-define([ './lib/bdParser', './lib/console' ], function (parse) {
-	parse(process.argv[3]);
+define([ './lib/bdParser', './lib/Module', './lib/node!fs', './lib/node!util', './lib/console' ], function (parse, Module, fs, util) {
+	require.rawConfig.commandLineArgs.slice(2).forEach(function processPath(parent, path) {
+		path = (parent + (path ? '/' + path : '')).replace(/\/{2,}/g, '/');
+		
+		try {
+			var stats = fs.statSync(path);
+		}
+		catch (error) {
+			console.error(error);
+			return;
+		}
+
+		if (stats.isDirectory()) {
+			fs.readdirSync(path).forEach(processPath.bind(this, path));
+		}
+		else if (stats.isFile() && /\.js$/.test(path)) {
+			parse(path);
+		}
+	});
+	
+	console.log("\nModules:\n", util.inspect(Module.getAll(), null, 6));
 });

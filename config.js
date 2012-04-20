8bd9e6ea99e37dad1aeaaaface398f513c79bb57
Colin Snover <github.com@zetafleet.com>
2012-04-20 14:27:48 +0000

    Add plumbing for pluggable environments, comment processing, and data exporting.

---
+++ config.js
@@ -0,0 +1,27 @@
+define({
+	// The execution environment(s) for the script being evaluated. (A minimal EcmaScript 5-compliant environment is
+	// always defined by default.) The following environments are provided by default:
+	//     - amd: An environment with an AMD-compatible loader
+	//     - browser: A Web browser environment
+	//     - node: A node.js environment
+	// Note that js-doc-parse currently expects an AMD environment and might not work correctly if one is omitted.
+	// Additional custom environments can be used by passing the module ID of a custom environment module.
+	environments: [ "amd", "browser" ],
+
+	// The inline documentation processor(s) to use. The following processors are provided by default:
+	//    - dojodoc: A parser for the Dojo documentation format
+	//    - jsdoc: A parser for the jsdoc documentation format
+	// Additional custom processors can be used by passing the module ID of a custom processor module.
+	processors: [ "dojodoc" ],
+
+	// The exporter(s) to use. The following exporters are provided by default:
+	//    - dojov1: An exporter for the v1 Dojo API browser.
+	// Additional custom exporters can be used by passing the module ID of a custom exporter module.
+	exporters: [ "dojov1" ],
+
+	// Configuration data for module ID resolution and path remapping within the parser.
+	moduleConfig: {
+		basePath: '',
+		prefixMap: {}
+	}
+});

f926ea7f6b0ebcb566da481e1a2e115122ca69f6
Colin Snover <github.com@zetafleet.com>
2012-05-30 20:57:45 +0000

    Parser needs to wait for environment to be ready before starting execution.

--- parse.js
+++ parse.js
@@ -7,50 +7,52 @@ define([
 	'./lib/console',
 	'./lib/esprimaParser'
 ], function (env, File, Module, fs, util, console) {
-	console.status('Processing scripts…');
+	env.ready(function(){
+		console.status('Processing scripts…');
 
-	require.rawConfig.commandLineArgs.slice(2).forEach(function processPath(parent, path) {
-		if (typeof path === 'number') {
-			path = '';
-		}
+		require.rawConfig.commandLineArgs.slice(2).forEach(function processPath(parent, path) {
+			if (typeof path === 'number') {
+				path = '';
+			}
 
-		path = (parent + (path ? '/' + path : '')).replace(/\/{2,}/g, '/');
-		var stats;
+			path = (parent + (path ? '/' + path : '')).replace(/\/{2,}/g, '/');
+			var stats;
 
-		try {
-			stats = fs.statSync(path);
-		}
-		catch (error) {
-			console.error(error);
-			return;
-		}
-
-		if (stats.isDirectory()) {
-			fs.readdirSync(path).sort().forEach(processPath.bind(this, path));
-		}
-		else if (stats.isFile() && /\.js$/.test(path)) {
-			// TODO: This whole thing revolves around Modules because that's what an AMD system uses, but we really
-			// ought to isolate modules to the AMD callHandler so this tool can be used as an even more general
-			// documentation parser.
-
-			// Skip excluded paths
-			if (env.config.excludePaths.some(function (exclude) {
-				return typeof exclude === 'string' ?
-					path.indexOf(exclude) === 0 :
-					exclude.test(path);
-			})) {
+			try {
+				stats = fs.statSync(path);
+			}
+			catch (error) {
+				console.error(error);
 				return;
 			}
 
-			Module.getByFile(new File(fs.realpathSync(path)));
-		}
+			if (stats.isDirectory()) {
+				fs.readdirSync(path).sort().forEach(processPath.bind(this, path));
+			}
+			else if (stats.isFile() && /\.js$/.test(path)) {
+				// TODO: This whole thing revolves around Modules because that's what an AMD system uses, but we really
+				// ought to isolate modules to the AMD callHandler so this tool can be used as an even more general
+				// documentation parser.
+
+				// Skip excluded paths
+				if (env.config.excludePaths.some(function (exclude) {
+					return typeof exclude === 'string' ?
+						path.indexOf(exclude) === 0 :
+						exclude.test(path);
+				})) {
+					return;
+				}
+
+				Module.getByFile(new File(fs.realpathSync(path)));
+			}
+		});
+
+		console.status('Exporting results…');
+
+		env.exporters.forEach(function (exporter) {
+			exporter.run(exporter.config);
+		});
+
+		console.status('Done!');
 	});
-
-	console.status('Exporting results…');
-
-	env.exporters.forEach(function (exporter) {
-		exporter.run(exporter.config);
-	});
-
-	console.status('Done!');
 });
